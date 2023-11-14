ece22d331bfb7e0a243d631583333512bc186d2d
Ruben Martinez <rubenmrtnz2@gmail.com>
2023-11-14 08:22:56 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//the numbers for array 
+const start = 10;
+const end = 30;
+//How to tget even numbers
+const evenNumbers = [];
+for (let i = start; i <= end; i += 2) {
+    evenNumbers.push(i);
+}
+console.log(evenNumbers);
+
